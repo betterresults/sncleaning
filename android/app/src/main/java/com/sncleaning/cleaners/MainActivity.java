edf88dc7ef56e9c03f5e515bf28ab3d858039b17
Silvia Ivanova Nedeva <info@betterbusinessresults.co.uk>
2025-11-09 09:50:47 +0000

    Add Android platform for mobile app build

---
+++ android/app/src/main/java/com/sncleaning/cleaners/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sncleaning.cleaners;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sncleaning.cleaners',
+  appName: 'SN Cleaning',
+  webDir: 'dist'
+};
+
+export default config;
